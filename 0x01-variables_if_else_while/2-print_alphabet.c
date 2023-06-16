0eab095b13b598509bc3912aae63d3733afd8716
jaafarenassiri <enassirija@gmail.com>
2023-06-16 09:47:45 -0700

    work

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,11 +4,10 @@ int main(void) {
     int i;
   
     for (i = 0; i < 26; i++) {
-        putchar(a + i);
+        putchar("a" + i);
     }
   
-    putchar(n);
+    putchar("\n");
   
     return 0;
 }
-
