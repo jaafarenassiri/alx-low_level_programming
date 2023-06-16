e8fcb85afab8e797393ec5daa37d6a2f976e3b73
jaafarenassiri <133459389+jaafarenassiri@users.noreply.github.com>
2023-06-16 18:14:18 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,12 @@
  * Return: Always 0 (Success)
  *
  */
-int main(void) {
-    int i;
-
-    for (i = 0; i < 26; i++){
+int main(void){
+int i;
+for (i = 0; i < 26; i++){
         putchar('a' + i);
     }
-
-    putchar('\n');
-
-    return (0);
+putchar('\n');
+return (0);
 }
 
