aa23c5115bd14acd61a1cf484037054679d715ec
jaafarenassiri <133459389+jaafarenassiri@users.noreply.github.com>
2023-06-16 20:26:30 +0100

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int j;
+
+for (j = 0; j < 10; j++)
+{
+printf("%d", j);
+}
+
+printf("\n");
+
+return (0);
+}
