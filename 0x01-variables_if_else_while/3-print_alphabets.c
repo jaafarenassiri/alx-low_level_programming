9712f2508e88bce347394831626db36b57867c9c
jaafarenassiri <133459389+jaafarenassiri@users.noreply.github.com>
2023-06-16 20:17:31 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,12 +5,17 @@
   * Return: Always 0 (Success)
   *
   */
-int main(void){
+int main(void)
+{
 char lowercase, uppercase;
-for (lowercase = 'a'; lowercase <= 'z'; lowercase++){
-putchar(lowercase);}
-for (uppercase = 'A'; uppercase <= 'Z'; uppercase++){
-putchar(uppercase);}
+for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+{
+putchar(lowercase);
+}
+for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+{
+putchar(uppercase);
+}
 putchar('\n');
 return 0;
 }
