3e07d52cbd35ce511b9eb5f5df39038d3c0ebac6
jaafarenassiri <133459389+jaafarenassiri@users.noreply.github.com>
2023-06-20 20:37:59 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
-*print_alphabet-print all alphabet in lowercase
-*
-*Return : nothing
-*/
-void print_alphabet(void) {
-    char i;
+ * print_alphabet - prints the alphabet
+ *
+ * Return : nothing
+ */
+void print_alphabet(void)
+{
+char i;
     
-    for (i = 'a'; i <= 'z'; i++) 
-    {
-        _putchar(i);
-    }
-    _putchar('\n'); // Add a newline after printing the alphabet
+for (i = 'a'; i <= 'z'; i++) 
+{
+_putchar(i);
+}
+_putchar('\n'); 
 }
