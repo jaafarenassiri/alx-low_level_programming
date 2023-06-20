28b785939a2dbc562616f9416b230a07de830d5f
jaafarenassiri <133459389+jaafarenassiri@users.noreply.github.com>
2023-06-20 21:23:03 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,19 +8,21 @@
  */
 int print_sign(int n)
 {
+int p;
 if (n > 0)
 {
 _putchar('+');
-return (1);
+p=1;
 }
 else if (n < 0)
 {
 _putchar('-');
-return (-1);
+p=-1;
 }
 else
 {
 _putchar('0');
-return (0);
+p=0;
 }
+return (p);
 }
