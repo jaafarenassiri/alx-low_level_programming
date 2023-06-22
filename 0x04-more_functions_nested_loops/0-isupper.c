0c2765080966f2ca4fdb9cc98321b8f0de559e3c
jaafarenassiri <133459389+jaafarenassiri@users.noreply.github.com>
2023-06-22 18:02:57 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 int _isupper(int c) {
   if (c >= 'A' && c <= 'Z') {
-    return 1;  // The input corresponds to an uppercase letter.
+    return 1;  /* The input corresponds to an uppercase letter.*/
   } else {
-    return 0;  // The input does not correspond to an uppercase letter.
+    return 0;  /* The input does not correspond to an uppercase letter.*/
   }
 }
